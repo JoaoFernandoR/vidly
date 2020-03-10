9a074dde483c8c4f74ce455d11f7b5151a1732f1
Joao Fernando <joao.amorim@alumni.usp.br>
2020-03-10 14:03:43 -0300

    After using the Put method

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const Joi = require('@hapi/joi')
 const server = express()
 
+server.use(express.json())
+
+// esquema para o Joi
+const schema = Joi.object({
+    name : Joi.string().min(3).required()
+})
+
 courses = [
-    {id : 1, course : "course1" },
-    {id : 2, course : "course2" },
-    {id : 3, course : "course3" },
-    {id : 4, course : "course4" },
+    {id : 1, name : "course1" },
+    {id : 2, name : "course2" },
+    {id : 3, name : "course3" },
+    {id : 4, name : "course4" },
 ]
 
+// Rotas de GET
 server.get('/', (req, res) => {
     res.send("Página inicial")
 })
@@ -20,10 +29,56 @@ server.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
 
     if (!course) 
-       return res.send("O curso com o id em questão não foi encontrado em nosso sistema")
-       
+       return res.status(404).send("O curso com o id em questão não foi encontrado em nosso sistema")
+
     res.send(course)
 })
 
+// Rotas de post
+
+server.post('/api/courses', (req, res) => {
+
+    const result = schema.validate(req.body)
+
+    if(result.error) 
+    // Bad request
+        return res.status(400).send(result.error.details[0].message)
+
+    // if (!req.body.name)
+    //     return res.status(400).send("name of the course doesnt exist")
+
+    // if (req.body.name.length === 0 || req.body.name.length === 1 )
+    //     return res.status(400).send('Name of the course too short')
+
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    }
+
+    courses.push(course)
+    res.send(course)
+})
+
+//Rotas de put
+server.put('/api/courses/:id', (req, res) => {
+
+    const result = schema.validate(req.body)
+
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course)
+        return res.status(404).send("Não há um curso com esse ID.")
+
+    if (result.error)
+        return res.status(400).send(result.error.details[0].message)   
+
+    // encontrando o indíce na lista de onde foi encontrado o item com o id requerido    
+    indicedalista = courses.indexOf(course)
+    console.log(indicedalista)
+    courses[indicedalista].name  = req.body.name
+
+    res.send(courses)
+
+})
 
 server.listen(3002, () => console.log('Escutando na porta 3002...'))
