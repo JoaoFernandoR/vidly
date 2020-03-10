fa7e02ae92651ca3c1b278a6952bee9e21320334
Joao Fernando <joao.amorim@alumni.usp.br>
2020-03-10 11:36:06 -0300

    After the Get routes

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const server = express()
 
-meuteste = {
-    ok : true
-}
+courses = [
+    {id : 1, course : "course1" },
+    {id : 2, course : "course2" },
+    {id : 3, course : "course3" },
+    {id : 4, course : "course4" },
+]
 
 server.get('/', (req, res) => {
-    res.send(meuteste)
+    res.send("Página inicial")
+})
+
+server.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+server.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+
+    if (!course) 
+       return res.send("O curso com o id em questão não foi encontrado em nosso sistema")
+       
+    res.send(course)
 })
 
 
-
-server.listen(3000, () => console.log('Escutando na porta 3000...'))
+server.listen(3002, () => console.log('Escutando na porta 3002...'))
